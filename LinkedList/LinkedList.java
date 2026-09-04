38890d36ccdeb0f001bc10e9160cf91de531400c
J0sH-code <joshrylesanteno07@gmail.com>
2026-09-04 14:45:58 +0800

    refactor LinkedList class to use generics and add size management

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -1,3 +1,24 @@
-public class LinkedList {
+public class LinkedList<T> {
+    private Node<T> firstNode;
+    private Node<T> lastNode;
+    private int size;
+
+    public LinkedList() {
+    }
+
+    public LinkedList(int size) {
+        this.size = size;
+
+        for (int i = 0; i < this.size; i++) {
+            Node<T> node = new Node<>(null, null, null);
+            if (firstNode != null) {
+                lastNode.setNextNode(node);
+                lastNode = node;
+            } else {
+                firstNode = node;
+                lastNode = firstNode;
+            }
+        }
+    }
 
 }
