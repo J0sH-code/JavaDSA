db19540b5ec51cc84390d51e0fd5b74fe6a942f6
J0sH-code <joshrylesanteno07@gmail.com>
2026-09-04 14:45:50 +0800

    add Node class for LinkedList implementation

---
+++ LinkedList/Node.java
@@ -0,0 +1,37 @@
+public class Node<T> {
+    private T data;
+    private Node<T> nextNode;
+    private Node<T> previousNode;
+
+    public Node(T data, Node<T> nextNode, Node<T> previousNode) {
+        this.data = data;
+        this.nextNode = nextNode;
+        this.previousNode = previousNode;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Node<T> getNextNode() {
+        return nextNode;
+    }
+
+    public void setNextNode(Node<T> nextNode) {
+        this.nextNode = nextNode;
+    }
+
+    public Node<T> getPreviousNode() {
+        return previousNode;
+    }
+
+    public void setPreviousNode(Node<T> previousNode) {
+        this.previousNode = previousNode;
+    }
+
+    
+}
